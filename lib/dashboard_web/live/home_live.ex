5fe8cfad14ad0687a597bc43c7737f6376e84f9e
ivan minutillo <bernini@inventati.org>
2020-09-13 18:46:58 +0200

    day

--- lib/dashboard_web/live/home_live.ex
+++ lib/dashboard_web/live/home_live.ex
@@ -25,6 +25,7 @@ defmodule DashboardWeb.HomeLive do
 
   def convert_diet(sheet) do
     [_h | t] = sheet.list
+
     diet = t
     |> Enum.map(fn x -> convert_to_struct(x) end)
     |> Enum.group_by(&(&1.name)) # groups as specified
@@ -46,20 +47,48 @@ defmodule DashboardWeb.HomeLive do
     end)
   end
 
+  def nutritional_value(sheet, value) do
+    sheet
+      |> Enum.map(fn x -> x[value] end)
+      |> Enum.sum()
+  end
+
+
+  def extract_nutrition(sheet) do
+    [h | t] = sheet.list
+    t
+    |> Enum.map(fn x ->
+      [name, per, kcal, grassi, saturated, carbo, sugar, protein, salt, fiber, q] = x
+      %{name: name, kcal: kcal / per * q |> Float.ceil(2)   , grassi: String.to_float(grassi) / per * q |> Float.ceil(2)  , saturated: String.to_float(saturated) / per * q |> Float.ceil(2)  , carbo: String.to_float(carbo) / per * q |> Float.ceil(2)  , sugar: String.to_float(sugar) / per * q |> Float.ceil(2)  , protein: String.to_float(protein) / per * q |> Float.ceil(2)  , salt: String.to_float(salt) / per * q |> Float.ceil(2)  , fiber: String.to_float(fiber) / per * q |> Float.ceil(2)  , q: q}
+    end)
+
+  end
 
   def mount(_params, _session, socket) do
     sheets = multi_extract(path())
     sheets = Enum.map(sheets, fn x -> extract_sheet(x) end)
+
+    day = sheets
+    |> Enum.find(fn x -> x[:name] === "giornata tipo" end)
+    |> extract_nutrition()
+    IO.inspect(day)
+
     diet = sheets
     |> Enum.find(fn x -> x[:name] === "Programma alimentare" end)
     |> convert_diet()
-    IO.inspect(diet)
     {:ok, assign(
       socket,
       misure: sheets
       |> Enum.find(fn x -> x[:name] === "Misure antropometriche" end)
       |> Jason.encode!,
       diet: diet,
+      day: day,
+      kcal: nutritional_value(day, :kcal) |> Float.ceil(2) ,
+      fat: nutritional_value(day, :grassi) |> Float.ceil(2) ,
+      carb: nutritional_value(day, :carbo) |> Float.ceil(2) ,
+      protein: nutritional_value(day, :protein) |> Float.ceil(2) ,
+      fiber: nutritional_value(day, :fiber) |> Float.ceil(2) ,
+      sugar: nutritional_value(day, :sugar) |> Float.ceil(2)
       # colatione: colazione
       # spuntino: spuntino
       # principale: principale
